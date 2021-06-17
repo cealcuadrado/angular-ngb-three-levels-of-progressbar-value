65d70f37171b4bd3be3f78cfa4c58219b66a9fa3
CeAlCuadrado <cealcuadrado@outlook.com>
2021-06-17 12:05:25 -0400

    Definida distribución gráfica y comportamiento del layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  nivelTamannio = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  definirNivelTamannio(): string {
+    return `nivel-${this.nivelTamannio}`;
+  }
+
+  definirNivelBarraProgreso(): string {
+    switch (this.nivelTamannio) {
+      case 1: { return `2rem`; break;}
+      case 2: { return `2.1rem`; break;}
+      case 3: { return `2.5rem`; break;}
+    }
+  }
 }
